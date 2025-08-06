ebe8b00d62fa1dcefe7f96a25d4dae218b90571f
wkdwlgns12 <jjg530@kbu.ac.kr>
2025-08-06 10:25:12 +0900

    init

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
-const bookRoutes = require("./routes/books")
-
 
 app.use(express.json())
-app.use("/books", bookRoutes)
-
-
-
 
 
 app.get("/", (req, res) => {
-    res.send("Hello Expreess!")
+    res.send("Hello Express!")
 })
 app.listen(PORT, () => {
     console.log("Server is running!")
---
+++ models/boardModel.js
@@ -0,0 +1,17 @@
+// models/boardModel.js
+
+// 게시판 데이터 배열 정의
+const Board = [
+    {
+        id: 1,
+        title: "공지사항",
+        content: "환영합니다!"
+    },
+    {
+        id: 2,
+        title: "업데이트",
+        content: "버전 1.0 출시!"
+    }
+];
+
+module.exports = Board;
